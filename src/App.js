dc6c1d05633df20c2a1f7b482d33aa9b459a3f5f
Praveen Chandran <pc-mac@Praveens-MacBook-Air.local>
2020-01-19 17:17:43 +0400

    router for hatss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Switch,Route} from 'react-router-dom';
+
+function Hats(props){
+  console.log(props);
+  return(
+    <div>
+      Hats Page
+    </div>
+
+  )
+}
 
 function App() {
   return (
     <div >
-      
-      <HomePage/>
+      <Switch>
+      <Route exact path='/' component={HomePage} ></Route>
+      <Route path='/shop/hats' component={Hats}></Route>
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import {withRouter} from 'react-router-dom';
 
-const MenuItem = ({title,imageUrl,size}) => {
+const MenuItem = ({title,imageUrl,size,history,linkUrl,match}) => {
     return (
         
             <div style={{backgroundImage:`url(${imageUrl})`}} 
-            className={`${size} menu-item`}>
+            className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
                     <div className="content">
                         <h1 className="title">{title.toUpperCase()}</h1>
                         <span className="subtitle">SHOP NOW</span>
@@ -15,4 +16,4 @@ const MenuItem = ({title,imageUrl,size}) => {
     );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
